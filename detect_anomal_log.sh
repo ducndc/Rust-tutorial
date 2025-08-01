2ccce0eafd192ce9a5edb2e29bb2bad8fd4e2153
ducndc <nguyendangchungduc1999@gmail.com>
2025-08-01 17:44:56 +0700

    auto detect log

--- detect_anomal_log.sh
+++ detect_anomal_log.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+echo "Parse log"
 cd ./log-parser
-
 python parse_log_with_drain.py
 
+echo "analaysis log"
 cd ../loganalysis
-
 python test.py
 
+echo "view result"
 libreoffice --calc ./results/anomalous_events_with_templates.csv
