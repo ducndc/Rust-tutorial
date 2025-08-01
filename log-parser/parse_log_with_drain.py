070257bd29517f4fab447fdfddde31de6d618e25
ducndc <nguyendangchungduc1999@gmail.com>
2025-08-01 15:08:17 +0700

    update format log

--- log-parser/parse_log_with_drain.py
+++ log-parser/parse_log_with_drain.py
@@ -8,7 +8,9 @@ input_dir = '../data/input_log/' # The input directory of log file
 output_dir = '../data/result/'  # The output directory of parsing results
 log_file = 'logmesh'  # The input log file name
 #log_format = '<Date> <Time> <Level> <Component>: <Content>'  # wifi log format
-log_format = '<Timestamp> <Level> <Component>: <Code> <Tag> <Content>'
+#log_format = '<Timestamp> <Level> <Component>: <Code> <Tag> <Content>'
+
+log_format = '<Day> <Month> <Date> <Time> <Year> <Level> <Component>: <Line> <Content>'
 
 # Regular expression list for optional preprocessing (default: [])
 # regex      = [
@@ -22,6 +24,12 @@ regex = [
     r'(?<=[^A-Za-z0-9])[-+]?[0-9]+(?=[^A-Za-z0-9])|[0-9]+$', # Numbers
     r'\[[0-9]+\]',                                # các đoạn như [887]
     r'\[[A-Z]+\]',                                # các đoạn như [TOPO], [INFO]
+    r'\[\d+\]',          # Mã như [104]
+    r'\[[A-Za-z0-9]+\]', # Tên module như [p1905]
+    r'(?<= )\d+(?= )',   # Các số như 30            
+    r'\[[^\]]+\]',               
+    r'(?<=[^\w])\d+(?=[^\w])',  
+    r'(\d+\.){3}\d+', 
 ]
 
 st         = 0.5  # Similarity threshold
